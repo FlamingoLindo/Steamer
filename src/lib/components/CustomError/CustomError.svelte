c4328ba84f0e86efb7c7c1f5518bd435568d774c
FlamingoLindo <vitorantunes2003@gmail.com>
2025-12-14 11:50:19 -0300

    feat: custom error

---
+++ src/lib/components/CustomError/CustomError.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { CustomToast } from '$lib/toast/custom.toast';
+
+	const { message, error }: { message: string; error: string } = $props();
+</script>
+
+<div class="flex items-center justify-center py-12">
+	{CustomToast.show({
+		type: 'error',
+		message: message,
+		position: 'top-right'
+	})}
+	<div class="rounded-lg border-2 border-red-500 bg-[#1b2838] px-8 py-6 shadow-lg">
+		<p class="font-semibold text-red-400">{message}: {error}</p>
+	</div>
+</div>
